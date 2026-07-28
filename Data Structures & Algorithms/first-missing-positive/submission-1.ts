aa442f0239b93ae3038937d66b2e91a3c83207fa
Oroja Samuel Adeyemi <samueloroja123@gmail.com>
2026-07-28 13:08:35 +0100

    Add: first-missing-positive - submission-1

---
+++ Data Structures & Algorithms/first-missing-positive/submission-1.ts
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    firstMissingPositive(nums: number[]): number {
+        for(let i = 0;i<nums.length;i++){
+            while(
+                nums[i]>0 && nums[i]<= nums.length && nums[i] !==i+1 && nums[nums[i]-1] !==nums[i]
+            ){
+                const targetIndex = nums[i]-1;
+                [nums[i],nums[targetIndex]] = [nums[targetIndex],nums[i]]
+            }
+        }
+        for(let i =0;i< nums.length;i++){
+            if(nums[i]!==i+1){
+             return i+1
+            }
+        }
+    return nums.length +1
+    }
+}
