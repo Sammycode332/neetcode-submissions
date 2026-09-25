73a870cc318e38fc092eeae75ae17f07157a6955
Oroja Samuel Adeyemi <samueloroja123@gmail.com>
2026-09-25 09:45:03 +0100

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.ts
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs: string[]): string[][] {
+        //learnt mapping to sort every value in a list
+        const seen: Record<string,string[]> = {}
+        for(const str of strs){
+            const key = str.split('').sort().join('')
+
+            if(!seen[key]){
+                seen[key] = []
+            }
+            seen[key].push(str)
+        }
+        //learnt this to convert object to an array
+        return Object.values(seen)
+    }
+}
